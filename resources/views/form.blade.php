c8571dd345ab3baf362a5b2221e922e70452e65e
dyahutami2017 <dyahutami.2017@student.uny.ac.id>
2019-07-27 15:40:17 +0700

    mengedit tampilan form pertama dan mengedit table tapi table baru sedikit

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -3,11 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Promo Privatq</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}"> 
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">   
   </head>
   <body>
+<div class="logo"></div>
     <div class="FormField">
-      <h1>Isi Form Berikut</h1>
+      <h1>Isian Form</h1>
       <form action="/store" method="post">
       {{csrf_field()}}
         <input type="text" name="nama" placeholder="Nama Lengkap"/>
@@ -15,7 +17,10 @@
         <input type="tel" name="telepon" placeholder="Nomor HP"/>
         <br>
         <input class="FormField__Button" type="submit" name="" value="Kirim">
-
-    </div>
+        
+      </div>
+      
+    
   </body>
+  
 </html>
