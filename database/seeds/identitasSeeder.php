dd9f6e6f416a73247536c3349df6fced84dfcf24
dyahutami2017 <dyahutami.2017@student.uny.ac.id>
2019-07-29 17:33:08 +0700

    menambah data faker

--- database/seeds/identitasSeeder.php
+++ database/seeds/identitasSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 class identitasSeeder extends Seeder
 {
     /**
@@ -11,6 +11,13 @@ class identitasSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker=Faker::create('id_ID');
+        for($i = 1; $i<=50;$i++){
+            DB::table('identitas')->insert([
+                'nama'=>$faker->name,
+                'email'=>$faker->email,
+                'telepon'=>$faker->phoneNumber,
+            ]);
+        }
     }
 }
