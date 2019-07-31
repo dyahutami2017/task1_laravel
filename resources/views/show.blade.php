65a38f0118561a95f98235eb609a8881a79afb88
Gitasp <gita.220725@gmail.com>
2019-07-31 16:16:37 +0700

    menambahkan background

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -3,54 +3,9 @@
 <head>
 	<title>Promo Privatq</title>
 	
-	<link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/data.css') }}">
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-	<style type="text/css">
-	body{
-		background-color:#52C4B9;
-		
-	}
-	h1{
-		text-align:center;
-		font-family:'Raleway';
-		padding:30px;
-	}
 
-	table{
-		width:100%;
-		
-		padding:5px;
-		margin:auto;
-		background: white;
-  border-radius:3px;
-  border-collapse: collapse;
-  height: 100%;
-  margin: auto;
-  max-width: 600px;
-  padding:5px;
-  width: 100%;
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  animation: float 5s infinite;
-}
-
-th {
-  color:#D5DDE5;;
-  background:#1b1e24;
-  border-bottom:4px solid #9ea7af;
-  border-right: 1px solid #343a45;
-  font-size:23px;
-  font-weight: 100;
-  padding:24px;
-  text-align:left;
-  text-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
-  vertical-align:middle;
-}
-tr:hover {
-  background:#4E5066;
-  color:#FFFFFF;
-  border-top: 1px solid #22262e;
-}
-	</style>
     
 	
 </head>
