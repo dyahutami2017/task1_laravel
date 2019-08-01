c76335a09b7db89e59532f0cd39558cc90599f8a
Gitasp <gita.220725@gmail.com>
2019-08-01 15:07:39 +0700

    fe data

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,30 +2,29 @@
 <html>
 <head>
 	<title>Promo Privatq</title>
-	<link rel="stylesheet" type="text/css" href="{{ asset('/css/data.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/data.css') }}">
+	
 	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
 	
     
 	
 </head>
 <body>
-<div class="color1">
-	
+<div class="color1 container">
+	<div class="col"> 
 	<h1>Data User</h1>
 
-	
-	
 	<br/>
 	<form action='/data/cari' method='GET'>
-	<div class='form-row align-items-center'>
-	
-		<input class='form-control col-md-2' type='text' name ='cari' placeholder='Ketikkan Nama' value='{{old("cari")}}'>
-		<div class="col-auto">
-		<input class='btn btn-primary' type='submit' value='Cari'>
+		<div class='form-row align-items-center'>
+		
+			<input class='form-control col' type='text' name ='cari' placeholder='Ketikkan Nama' value='{{old("cari")}}'>
+				<div class="col">
+					<input class='btn btn-primary' type='submit' value='Cari'>
+				</div>
 		</div>
-		</div>
-		</form>
+	</form>
 	<br/>
 	<table class="table">
 		<thead class="thead-black">
@@ -46,8 +45,10 @@
 		@endforeach
 	</table>
 	{{$user->links()}}
-    <a href="/">Kembali</a>
-	</div>
+
+    <a class="btn-outline-primary btn" href="/">Kembali</a>
+</div>
+</div>
 </body>
 </html>
 
